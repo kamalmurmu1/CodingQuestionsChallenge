2601fe45318487199c7778eefd4a83a4bf30a63d
kamalmurmu1 <kamalmurmu420@gmail.com>
2026-04-24 08:56:17 +0530

    longestCommonPrefix

---
+++ LeetCode/longestCommonPrefix/LongestCommonPrefix.java
@@ -0,0 +1,13 @@
+package LeetCode.longestCommonPrefix;
+
+public class LongestCommonPrefix {
+    public String longestCommonPrefix(String[] strs) {
+        String result = "";
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
